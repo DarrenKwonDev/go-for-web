56de9c3020393d0c564198eba68ae4d7e79d7c6d
DarrenKwonDev <anatomy1545@gmail.com>
2020-12-04 16:13:45 +0900

    tidy

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+// hotdog라는 타입
+type hotdog int
+
+var b hotdog
+
 func main() {
-	fmt.Println("this is playground")
+	var a int
+	b = 3
+	// a = b  error! main.hotdog를 int에 할당할 수 없음
+	a = int(b)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Printf("%T", b) // main.hotdog
 }
